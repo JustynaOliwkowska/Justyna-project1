dcb071d2400b396a8e5acdd45213b5cf5a40e5d8
JustynaOliwkowska <justynapastuszak@interia.eu>
2020-04-05 23:40:38 +0200

    cwiczenia

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,6 @@
 #exercise1
+from typing import List
+
 print("Mam na imie Justyna Oliwkowska")
 #exercise2
 print (2+3, 10/4, 30*2)
@@ -9,4 +11,63 @@ print (30*2)
 print("Hello World")
 print('Hello World')
 print(2)
-print(hello)
+
+cat = 'Garfield'
+result = "{} has a cat name {}.".format("Ala", cat)
+print(result)
+# this is next exercise typy danych
+#zmienna liczby calkowietej
+wiek = 30
+#zmienna  typu fload
+pi =3.14
+#zmienna typu str
+imie ='Justyna'
+#zmienna wielolinijkowy napis
+agenda = '''
+lalalala
+lalalala
+'''
+#zmienna wartosc logiczna
+result_1 = 9/2
+
+#zadanie 2
+k=2
+b=6
+c = k+b
+print(c)
+c=k+b
+print(c)
+
+#zadanie 3
+d = 5
+f = 1.6
+e = d + f
+print(e)
+#zadanie 4 - mam problem napisac kod dla a1 a2
+numbers = [1, 2]
+zmienna_pierwsza = "a", numbers[0]
+zmienna_druga = "a", numbers[1]
+print(zmienna_pierwsza, zmienna_druga)
+
+a1 = 10
+a2 = 15
+sum_value = a1 +a2
+quotus = a2 / a1
+int_part =a2 // a1
+print(sum_value, quotus, int_part)
+
+#zadanie 5
+animals = []
+animals.append('kot')
+print(animals)
+animals.append('slon')
+print(animals)
+animals.append('krowa')
+print(animals)
+#zadanie 6
+tablica = [1,2,3,4,5,6,7,8]
+print(tablica[-2])
+#zadanie7
+characters = ["Harry", "Ron", "Hermione"]
+print(characters[0], characters[-1])
+
