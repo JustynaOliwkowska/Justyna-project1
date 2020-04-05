fc2bb6edaa053d9f3ea4f621631e236f14544cae
JustynaOliwkowska <justynapastuszak@interia.eu>
2020-04-05 14:19:26 +0200

    this is test

---
+++ exercise_1.py
@@ -0,0 +1,12 @@
+#exercise1
+print("Mam na imie Justyna Oliwkowska")
+#exercise2
+print (2+3, 10/4, 30*2)
+print(2+3)
+print(10/4)
+print (30*2)
+#exercise3
+print("Hello World")
+print('Hello World')
+print(2)
+print(hello)
