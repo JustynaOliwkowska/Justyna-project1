d684e0d4f5e5b4cf3c96a8d4a439411534fb48cc
JustynaOliwkowska <justynapastuszak@interia.eu>
2020-04-09 23:12:05 +0200

    test

---
+++ exercise_3.py
@@ -0,0 +1,34 @@
+#petla for
+
+data = [ 2, 4, 6]
+
+for element in data:
+    print(f'Element : {element}')
+
+heroes = ["Luke Skywalker", "Princess Leia", "Han Solo", "Chewbacca", "Obi-Wan Kenobi"]
+
+for charac in heroes:
+    print(charac)
+
+for J in range (6):
+    print(J)
+
+for K in range (5, 10):
+    print(K)
+
+for L in range (3, 10, 2):
+    print(L)
+
+result = []
+for s in range(4):
+    result.append(s)
+print(result)
+
+resu = list(range(4))
+print(resu)
+
+#petla while
+i = 0
+while i < 10:
+    print(i)
+    i += 1
---
+++ exercise_4.py
@@ -0,0 +1,12 @@
+#zadanie 1
+k= 0
+while k < 10:
+    print('jestem programista')
+    k+= 1
+
+#zadanie 2
+y = 2
+for i in range(0, 11):
+    print(y**i)
+    i += 1
+
